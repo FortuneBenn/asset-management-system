671f7ab5457422575ccddceba723696314afdd1a
FortuneBenn <fortuneben69@gmail.com>
2024-11-29 11:12:19 +0200

    version 2

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', SignOutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),  # View user profile
-    path('profile/edit/', UserProfileUpdateView.as_view(), name=' profile_edit'),  # Edit user profile
+    path('profile/edit/', UserProfileUpdateView.as_view(), name='profile_edit'),  # Edit user profile
     path('staff/profile/', StaffProfileView.as_view(), name='staff_profile'),  # View user profile
     path('staff/profile/edit/', StaffProfileUpdateView.as_view(), name='staff_profile_edit'),  # Edit user profile 
      
